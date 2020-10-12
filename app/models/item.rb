3c04773711f90cfd82e8d37518d7279ca6aa1e0a
minisera <koki.ohtaka0821@gmail.com>
2020-10-13 08:04:08 +0900

    status_id ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :status
+
+  validates :user_id,:name,:comment,:price,:status_id,:category_id,:source_id,:charge_id,:day_id, presence: true
+
+  validates :status_id, numericality: {other_than: 1}
 end
