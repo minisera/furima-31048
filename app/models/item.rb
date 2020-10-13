25521cd273ee02788ac78883dbe47663518d4099
minisera <koki.ohtaka0821@gmail.com>
2020-10-13 09:28:48 +0900

    category追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :status
+  belongs_to_active_hash :status,:category
 
   validates :user_id,:name,:comment,:price,:status_id,:category_id,:source_id,:charge_id,:day_id, presence: true
 
   validates :status_id, numericality: {other_than: 1}
+  validates :category_id, numericality: {other_than: 1}
 end
