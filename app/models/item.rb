4382eec5aa1bb7d060a9cedbb5f22461669d2b81
minisera <koki.ohtaka0821@gmail.com>
2020-10-13 15:55:55 +0900

    gem 'mini_magick' gem 'image_processing', '~> 1.2'追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :status,:category,:charge,:source,:day
+  belongs_to :user
+  has_one_attached :image
 
   validates :user_id,:name,:comment,:price,:status_id,:category_id,:source_id,:charge_id,:day_id, presence: true
 
@@ -9,4 +11,5 @@ class Item < ApplicationRecord
   validates :charge_id, numericality: {other_than: 1}
   validates :source_id, numericality: {other_than: 1}
   validates :day_id, numericality: {other_than: 1}
+
 end
