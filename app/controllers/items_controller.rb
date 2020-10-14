8a3d8b42b5cf57f373c76c134cf398ead360a751
minisera <koki.ohtaka0821@gmail.com>
2020-10-14 17:56:12 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index,only: :edit
-  before_action :set_item,only: [:show,:edit,:update]
+  before_action :set_item,only: [:show,:edit,:update,:destroy]
+
   def new
     @item = Item.new
   end
-
+  
+  
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -14,17 +16,17 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
+  
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
   end
-
+  
   def show
   end
-
+  
   def edit
   end
-
+  
   def update
     if @item.update(item_params)
       redirect_to item_path(@item)
@@ -32,21 +34,27 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+  
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
-
+  
   def item_params
     params.require(:item).permit(:name, :comment, :price, :status_id, :category_id, :charge_id, :source_id, :day_id, :image).merge(user_id: current_user.id)
   end
-
+  
   def move_to_index
     @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
-
+  
   def set_item
     @item = Item.find(params[:id])
   end
+  
 end
